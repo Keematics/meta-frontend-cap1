c315bfa4d3b35164f28340a4cf67a259e53e002c
Keematics <agbajeabdulakeems@gmail.com>
2023-02-07 16:39:30 +0100

    add the open graph protocols to public/index.html, add semantic html tags to the individual components in the src/components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+        <img src='' alt='' />
+        <ul>
+            <li><a href=''>Home</a></li>
+            <li><a href=''>About</a></li>
+            <li><a href=''>Menu</a></li>
+            <li><a href=''>Reservations</a></li>
+            <li><a href=''>Order online</a></li>
+            <li><a href=''>Login</a></li>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
